4f6f34423b2d52a8e2b2598f9a806c43d56b1536
Gabrielassis134 <gabrielassisbrasil8gmail.com>
2026-01-07 12:08:37 -0300

    filtros, update IndexDB, ordenação e confirmação para excluir

--- js/storage.js
+++ js/storage.js
@@ -1,10 +1,41 @@
-const STORAGE_KEY = "tasks";
+const DB_NAME = "taskDB";
+const DB_VERSION = 1;
+const STORE_NAME = "tasks";
 
-function loadTasks() {
-   const data = localStorage.getItem(STORAGE_KEY);
-   return data ? JSON.parse(data) : [];
+function openDB() {
+   return new Promise((resolve, reject) => {
+      const request = indexedDB.open(DB_NAME, DB_VERSION);
+
+      request.onupgradeneeded = (e) => {
+         const db = e.target.result;
+         if (!db.objectStoreNames.contains(STORE_NAME)) {
+            db.createObjectStore(STORE_NAME, { keyPath: "id" });
+         }
+      };
+
+      request.onsuccess = () => resolve(request.result);
+      request.onerror = () => reject(request.error);
+   });
 }
 
-function saveTasks(tasks) {
-   localStorage.setItem(STORAGE_KEY, JSON.stringify(tasks));
+async function getAllTasks() {
+   const db = await openDB();
+   return new Promise((resolve) => {
+      const tx = db.transaction(STORE_NAME, "readonly");
+      const store = tx.objectStore(STORE_NAME);
+      const request = store.getAll();
+      request.onsuccess = () => resolve(request.result);
+   });
+}
+
+async function saveTask(task) {
+   const db = await openDB();
+   const tx = db.transaction(STORE_NAME, "readwrite");
+   tx.objectStore(STORE_NAME).put(task);
+}
+
+async function deleteTaskDB(id) {
+   const db = await openDB();
+   const tx = db.transaction(STORE_NAME, "readwrite");
+   tx.objectStore(STORE_NAME).delete(id);
 }
